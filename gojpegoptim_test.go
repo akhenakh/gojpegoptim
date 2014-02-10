e6a6248b9f92e8a92faa86d05fc5adfc1646b1d0
Fabrice Aneche <akh@nobugware.com>
2014-02-10 13:13:33 -0800

    working release

--- gojpegoptim_test.go
+++ gojpegoptim_test.go
@@ -1,16 +1,25 @@
 package gojpegoptim
 
 import (
-	"os"
+	"bytes"
+	"image/jpeg"
+	"io/ioutil"
 	"testing"
 )
 
 func TestJpegOptimFromBuffer(t *testing.T) {
-	fi, err := os.Open("test.jpg")
+	fi, err := ioutil.ReadFile("test.jpg")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer fi.Close()
-	imgBytes, err := gojpegoptim.EncodeOptimized(fi)
+	imgBytes, err := EncodeOptimized(fi, 100)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = jpeg.Decode(bytes.NewReader(imgBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	t.Log("input size", len(fi), "output size", len(imgBytes), "gain", len(fi)-len(imgBytes), (len(fi)-len(imgBytes))*100/len(fi), "%")
 }
