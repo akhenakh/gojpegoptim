1a6efe3b02574eab8907826d884d78c9857bed42
Fabrice Aneche <akh@nobugware.com>
2014-02-10 13:15:49 -0800

    fix compilation, added test img

--- c-gojpegoptim.c
+++ c-gojpegoptim.c
@@ -183,38 +183,3 @@ void optimizeJPEG(unsigned char *inputbuffer, unsigned long inputsize, unsigned 
   }
 }
 
-int main() {
-
-  FILE *fp;
-  fp = fopen("test.jpg","r"); // read mode
-  if (fp == NULL)  {
-    printf("fuck\n");
-    exit(0);
-  }
-  fseek (fp , 0 , SEEK_END);
-  
-  unsigned long fsize;
-  fsize = ftell(fp);
-  rewind(fp);
-
-  unsigned char *jpg_inputbuffer;
-  unsigned char *jpg_outputbuffer = NULL;
-
-  jpg_inputbuffer = (unsigned char*) malloc(fsize + 100);
-  fread(jpg_inputbuffer, 1, fsize, fp);
-  printf("input size: %lu\n", fsize);
-  unsigned long ouputsize = 0;
-  optimizeJPEG(jpg_inputbuffer, fsize, &jpg_outputbuffer, &ouputsize, -1);
-
-  fclose(fp);
-  printf("output size: %lu\n", ouputsize);
-
-  FILE *fo;
-  fo = fopen("output.jpg", "w");
-
-  fwrite(jpg_outputbuffer, 1, ouputsize, fo);
-
-  fclose(fo);
-  free(jpg_inputbuffer);
-  free(jpg_outputbuffer);
-}
